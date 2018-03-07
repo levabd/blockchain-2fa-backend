604e25f7744b7af970c21f1b1d30f35a9532ca8f
Peshkov Maxim <peshkov-maximum@yandex.ru>
2018-03-07 11:51:03 +0600

    Update example

--- frontend_api_key_generation_example/js-crc32-1.2.0/bits/10_types.js
+++
@@ -1,5 +0,0 @@
-/*::
-type CRC32Type = number;
-type ABuf = Array<number> | Buffer | Uint8Array;
-type CRC32TableType = Array<number> | Int32Array;
-*/
--- frontend_api_key_generation_example/js-crc32-1.2.0/ctest/fakeassert.js
+++
@@ -1,2 +0,0 @@
-var assert = {};
-assert.equal = function(x,y) { if(x !== y) throw new Error(x + " !== " + y); };
